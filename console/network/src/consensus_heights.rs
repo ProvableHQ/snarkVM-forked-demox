6707152cf6b9c7f1e1c05c2af3a7ddbcd25f4b1f
Victor Sint Nicolaas <vicsn@users.noreply.github.com>
2026-05-28 12:57:20 +0200

    Update mainnet V15 height

--- console/network/src/consensus_heights.rs
+++ console/network/src/consensus_heights.rs
@@ -141,7 +141,7 @@ pub const MAINNET_V0_CONSENSUS_VERSION_HEIGHTS: [(ConsensusVersion, u32); NUM_CO
     (ConsensusVersion::V12, 13_815_000),
     (ConsensusVersion::V13, 16_850_000),
     (ConsensusVersion::V14, 17_700_000),
-    (ConsensusVersion::V15, u32::MAX),
+    (ConsensusVersion::V15, 19_264_000),
 ];
 
 /// The consensus version heights for `TestnetV0`.
